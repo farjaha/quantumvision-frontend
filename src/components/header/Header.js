5e62e65c942064ea4378e404a6e9b14ac5d63600
Farzad Jahandar <farjaha@amazon.com>
2023-08-26 16:56:41 -0700

    remove all files

--- src/components/header/Header.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { Heading } from '@aws-amplify/ui-react';
-import './header.css'
-
-const welcome_message="Welcome to Quantum Vision File Access"
-export const Header = () => {
-  return <Heading level={1} textAlign={'center'} className='heading-style'>{welcome_message}</Heading>;
-};
