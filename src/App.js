d3d0299be38cc0e1c65690f7d26cd9ec8fb5d94c
Farzad Jahandar <farjaha@amazon.com>
2023-08-26 19:11:26 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { Home } from './components/home/Home';
+import { Logout } from './components/logout/Logout';
 import {Amplify} from 'aws-amplify';
 import awsconfig from './aws-exports';
 
 Amplify.configure(awsconfig);
 
+
+
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/logout" element={<Logout />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/components/logOut/Logout.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { Auth, Hub } from 'aws-amplify';
+import './logOut.css'; 
+
+const signInUrl = 'https://qv-auth.auth.us-east-2.amazoncognito.com/oauth2/authorize?client_id=3pssbadflhtmscuqtlbvbd08m3&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F';
+export const Logout = () => {
+  const [isSignedOut, setIsSignedOut] = useState(false);
+
+  useEffect(() => {
+    const checkAuthState = async () => {
+      try {
+        const user = await Auth.currentAuthenticatedUser();
+        setIsSignedOut(false);
+      } catch (error) {
+        setIsSignedOut(true);
+      }
+    };
+    checkAuthState();
+    const authListener = (data) => {
+      if (data.payload.event === 'signOut') {
+        setIsSignedOut(true);
+      }
+    };
+    Hub.listen('auth', authListener);
+    return () => {
+      Hub.remove('auth', authListener);
+    };
+  }, []);
+
+  const handleSignIn = () => {
+    // Redirect the user to the sign-in page
+    window.location.href = signInUrl;
+    console.log("logged out");
+  };
+  return (
+    <div className={`sign-out-box ${isSignedOut ? 'visible' : ''}`}>
+      {isSignedOut ? (
+        <div className="message-container">
+          <p className="message">You have successfully signed out.</p>
+          <p onClick={handleSignIn} className="sign-in-link">
+            Click here to sign in.
+          </p>
+        </div>
+      ) : null}
+    </div>
+  );
+};
