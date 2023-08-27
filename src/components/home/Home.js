dd91a71f58f521fcfa6506f31cea1d617c567105
Farzad Jahandar <farjaha@amazon.com>
2023-08-26 19:01:03 -0700

    revert

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,47 +2,67 @@ import { useEffect, useState } from 'react';
 import { Header } from '../header/Header';
 import { UserInfo } from '../user_info/UserInfo';
 // import { FilesList } from './files/FilesList';
-import { Auth } from 'aws-amplify';
-import { async } from 'q';
+import { Auth, Hub } from 'aws-amplify';
+
+const signInUrl = 'https://qv-auth.auth.us-east-2.amazoncognito.com/oauth2/authorize?client_id=3pssbadflhtmscuqtlbvbd08m3&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F';
 
 export const Home = () => {
-    const [isTokenExpired, setIsTokenExpired] = useState(false);
+    // const [isTokenExpired, setIsTokenExpired] = useState(false);
 
-    useEffect(() => {
-        const checkTokenExpiration = async () => {
-            try {
-                const user = await Auth.currentAuthenticatedUser();
-                const expirationTime = user.signInUserSession.idToken.payload.exp * 1000;
-                const currentTime = new Date().getTime();
+    // useEffect(() => {
+    //     const checkTokenExpiration = async () => {
+    //         try {
+    //             const user = await Auth.currentAuthenticatedUser();
+    //             const expirationTime = user.signInUserSession.idToken.payload.exp * 1000;
+    //             const currentTime = new Date().getTime();
 
-                if (currentTime >= expirationTime) {
-                    console.log("Token is expired!!!");
-                    setIsTokenExpired(true);
-                }
-                else {
-                    console.log("Token is still valid!!!");
-                }
-            } catch (error) {
-                console.error('Error checking token expiration:', error);
-            }
-        };
-        checkTokenExpiration();
-    })
-//   async function handleSignOut() {
-//     try {
-//       await Auth.signOut();
+    //             if (currentTime >= expirationTime) {
+    //                 console.log("Token is expired!!!");
+    //                 setIsTokenExpired(true);
+    //             }
+    //             else {
+    //                 console.log("Token is still valid!!!");
+    //             }
+    //         } catch (error) {
+    //             console.error('Error checking token expiration:', error);
+    //         }
+    //     };
+    //     checkTokenExpiration();
 
-//     } catch (error) {
-//       console.error('Error signing out:', error);
-//     }
-//   }
-  return (
-    <>
-      <Header />
-      <UserInfo />
-      {/* <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '50vh' }}>
-        <button onClick={handleSignOut} style={{ padding: '10px 40px', margin: '0 20px', border: 'none', background: 'grey' }}>Sign out</button>
-      </div> */}
-    </>
-  )
+    //     const authListener = (data) => {
+    //         if (data.payload.event === 'signOut') {
+    //             setIsTokenExpired(false);
+    //         }
+    //     };
+
+    //     Hub.listen('auth', authListener);
+
+    //     return () => {
+    //         Hub.remove('auth', authListener);
+    //     }
+
+    // }, []);
+
+    async function handleSignOut() {
+        try {
+            window.location.href = signInUrl;
+            await Auth.signOut();
+
+        } catch (error) {
+            console.error('Error signing out:', error);
+        }
+    }
+
+    // const handleSignIn = () => {
+    //     console.log("handleSignIn");
+    //     window.location.href = signInUrl
+    // };
+
+    return (
+        <>
+            <Header />
+            <UserInfo />
+          <button onClick={handleSignOut} style={{ padding: '10px 40px', margin: '0 20px', border: 'none', background: 'grey' }}>Sign out</button>
+        </>
+    )
 }
